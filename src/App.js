1a2df29f45d61c9e3ddca46500f0e2c9644251e7
Atul Choudhary <t-atul1.choudhary@paytmmoney.com>
2023-06-26 15:42:43 +0530

    seo testing

--- src/App.js
+++ src/App.js
@@ -47,11 +47,39 @@ function App() {
         <title>BrainJEE</title>
         <meta
           name="description"
-          content="Brainjee - An innovative online learning
+          content="An innovative online learning
             platform offering wide range of courses. Designed to make learning impactful."
         />
         <link rel="icon" href="/logo512.png" />
         <link rel="canonical" href="https://www.vaisage.com/" />
+        {/* Twitter meta tags below */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:image" content="" />
+        <meta
+          name="twitter:title"
+          content="Brainjee - An innovative online learning
+          platform offering wide range of courses."
+        />
+        <meta
+          name="twitter:description"
+          content="An innovative online learning
+          platform offering wide range of courses. Designed to make learning impactful."
+        />
+
+        {/* Facebook meta tags below */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://vaisage.com/" />
+        <meta
+          property="og:title"
+          content="Brainjee - An innovative online learning
+          platform offering wide range of courses."
+        />
+        <meta
+          property="og:description"
+          content="An innovative online learning
+          platform offering wide range of courses. Designed to make learning impactful."
+        />
+        <meta property="og:image" content="" />
       </Helmet>
 
       <Routes>
